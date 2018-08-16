407f6b7821e1c01b44e22d4dc05219115d6322c2
Tenju Paul <tenju@Tenjus-MacBook-Air.local>
2018-08-16 16:40:29 -0700

    fixed home page

--- server/models/product.js
+++ server/models/product.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose')
 
 var ProductSchema = new mongoose.Schema({
-    title: {type:String, required:[true, "Product title cannot be empty",],minlength: 4},
-    price: {type:Number, required:[true, "Price cannot be empty",]},
+    title: {type:String, required:[true, "Product title cannot be empty"],minlength: 4},
+    price: {type:Number, required:[true, "Price cannot be empty"]},
     image: {type:String},
 },{timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }})
 
